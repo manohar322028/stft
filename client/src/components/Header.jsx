be8f34492e4c7aac992214ae0a54a9d316594e8c
manohar322028 <manohardahal40@gmail.com>
2024-04-11 21:59:30 +0545

    server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,54 +1,65 @@
 import React from "react";
-import { Navbar, NavbarBrand } from "flowbite-react";
-
 import { Link, useLocation } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Header() {
   const location = useLocation();
+
   return (
-    <>
-      <Navbar
-        fluid
-        rounded
-        className="bg-gray-700 flex justify-center items-center"
-      >
-        <NavbarBrand as={Link} to="/" className="text-white pr-32">
-          <img src="dummy-logo.png" alt="Logo" className="w-12 h-12" />
-        </NavbarBrand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link
-            active={location.pathname === "/"}
-            className="text-white"
-          >
-            <Link to="/">Home</Link>
-          </Navbar.Link>
-          <Navbar.Link
-            active={location.pathname === "/news"}
-            className="text-white"
-          >
-            <Link to="/news">News</Link>
-          </Navbar.Link>
-          <Navbar.Link
-            active={location.pathname === "/notices"}
-            className="text-white"
-          >
-            <Link to="/notices">Notices</Link>
-          </Navbar.Link>
-          <Navbar.Link
-            active={location.pathname === "/about"}
-            className="text-white"
-          >
-            <Link to="/about">About</Link>
-          </Navbar.Link>
-          <Navbar.Link
-            active={location.pathname === "/downloads"}
-            className="text-white"
-          >
-            <Link to="/downloads">Downloads</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <nav
+      className={`bg-ivory flex justify-between items-center px-4 py-2 border border-gray-200 shadow-md rounded-md`}
+    >
+      {/* Logo */}
+      <div className="flex items-center">
+        <img src="logo.png" alt="Logo" className="w-12 h-12 mr-4" />
+        <Link to="/" className="text-black font-bold text-xl">
+          NNTA Bagmati
+        </Link>
+      </div>
+
+      {/* Navigation links */}
+      <ul className="hidden md:flex space-x-4">
+        <li
+          className={`px-2 py-1 text-black hover:text-blue-600 ${
+            location.pathname === "/" ? "font-bold text-red-600" : ""
+          }`}
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          className={`px-2 py-1 text-black hover:text-blue-600 ${
+            location.pathname === "/news" ? "font-bold text-red-600" : ""
+          }`}
+        >
+          <Link to="/news">News</Link>
+        </li>
+        <li
+          className={`px-2 py-1 text-black hover:text-blue-600 ${
+            location.pathname === "/notices" ? "font-bold text-red-600" : ""
+          }`}
+        >
+          <Link to="/notices">Notices</Link>
+        </li>
+        <li
+          className={`px-2 py-1 text-black hover:text-blue-600 ${
+            location.pathname === "/about" ? "font-bold text-red-600" : ""
+          }`}
+        >
+          <Link to="/about">About</Link>
+        </li>
+        <li
+          className={`px-2 py-1 text-black hover:text-blue-600 ${
+            location.pathname === "/downloads" ? "font-bold text-red-600" : ""
+          }`}
+        >
+          <Link to="/downloads">Downloads</Link>
+        </li>
+      </ul>
+
+      {/* Hamburger menu */}
+      <div className="md:hidden text-black focus:outline-none">
+        <RxHamburgerMenu />
+      </div>
+    </nav>
   );
 }
