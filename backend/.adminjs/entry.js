0a212674ea84cdc126a3f6c26a8b7c3443bc4e8e
manohar322028 <manohardahal40@gmail.com>
2024-07-29 16:51:43 +0545

    changed a little

--- backend/.adminjs/entry.js
+++ backend/.adminjs/entry.js
@@ -3,13 +3,13 @@ import ApproveMember from '../api/ApproveMember'
 AdminJS.UserComponents.ApproveMember = ApproveMember
 import RejectMember from '../api/RejectMember'
 AdminJS.UserComponents.RejectMember = RejectMember
-import UploadEditComponent from '../../../../nodevenv/public_html/stft/backend/20/lib/node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
+import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
 AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
-import UploadListComponent from '../../../../nodevenv/public_html/stft/backend/20/lib/node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
+import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
 AdminJS.UserComponents.UploadListComponent = UploadListComponent
-import UploadShowComponent from '../../../../nodevenv/public_html/stft/backend/20/lib/node_modules/@adminjs/upload/build/features/upload-file/components/UploadShowComponent'
+import UploadShowComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadShowComponent'
 AdminJS.UserComponents.UploadShowComponent = UploadShowComponent
-import ImportComponent from '../../../../nodevenv/public_html/stft/backend/20/lib/node_modules/@adminjs/import-export/lib/components/ImportComponent'
+import ImportComponent from '../node_modules/@adminjs/import-export/lib/components/ImportComponent'
 AdminJS.UserComponents.ImportComponent = ImportComponent
-import ExportComponent from '../../../../nodevenv/public_html/stft/backend/20/lib/node_modules/@adminjs/import-export/lib/components/ExportComponent'
+import ExportComponent from '../node_modules/@adminjs/import-export/lib/components/ExportComponent'
 AdminJS.UserComponents.ExportComponent = ExportComponent
